0cb5148bc937ea87b25e86dced1b5f0dbd7dba9a
Choogi <drewtex@gmail.com>
2022-11-14 13:01:57 -0600

    asset caching

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -18,7 +18,7 @@ export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.add({ todo: content });
+  const request = store.add({ id: id, jate: content });
   const result = await request;
   console.log('Data saved to the database', result);
 }
