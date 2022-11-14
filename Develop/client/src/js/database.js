2c8227c1acc4457c77f6b168b6e8e0826b023796
Choogi <drewtex@gmail.com>
2022-11-14 13:56:14 -0600

    install working

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -15,18 +15,20 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
-  const sliem = jateDb.transaction('jate', 'readwrite');
-  const store = sliem.objectStore('jate');
-  const request = store.put({ id: 1, jate: content });
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ jate: content });
   const result = await request;
   console.log('Data saved to the database', result);
 }
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  const jateDb = await openDB('jate', 'readonly');
-  const store = sliem.objectStore('jate');
-  const request = store.get(1);
+  console.log('GET all from the database');
+  const jatesDb = await openDB('jate', 1);
+  const tx = jatesDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
   const result = await request;
   console.log('result.value', result);
   return result;
